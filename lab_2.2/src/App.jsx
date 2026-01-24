9d6da04c27d6e84e2e76a0faa7321f6a6518f669
gulsezim <adilgulsezim04@icloud.com>
2026-01-24 12:44:12 +0500

    feat: initial project structure for labs

---
+++ lab_2.2/src/App.jsx
@@ -0,0 +1,14 @@
+import ProductList from "./task_2/ProductList"
+import Section from "./task_3/Section"
+
+function App(){
+  return(
+    <>
+    <Section title="Products">
+      <ProductList/>
+    </Section>
+    </>
+  )
+}
+
+export default App
