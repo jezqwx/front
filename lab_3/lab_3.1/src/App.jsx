198c03cb2ce6c24c36924ab37548cdc9c736f99c
gulsezim <adilgulsezim04@icloud.com>
2026-01-31 11:56:09 +0500

    chore: initialize React projects for Lab 3

---
+++ lab_3/lab_3.1/src/App.jsx
@@ -0,0 +1,13 @@
+import CounterApp from "./CounterApp.jsx";
+import "./App.css"
+
+function App(){
+  return(
+    <div>
+      <h1>Lab 3.1</h1>
+      <CounterApp/>
+    </div>
+  )
+}
+
+export default App;
